da516bf0715ef5a1efc51a58b5d47ae30951b13e
Abbetor <balbin.bordsson@gmail.com>
2026-09-16 12:11:51 +0200

    Move list display outside add block

--- Del-A.cs
+++ Del-A.cs
@@ -5,47 +5,46 @@ while (true)
 {   
     Console.WriteLine("Type 'D' for delete and 'ADD' to add (case sensitive)");
     string choice = Console.ReadLine();
-    if(choice == "ADD")
-    {
 
+    if (choice == "ADD")
+    {
         Console.WriteLine("Enter item name (numbers not accepted)");
         string item = Console.ReadLine();
-    
+
         int number;
 
         if (!int.TryParse(item, out number))
         {   
-            
             string priceOfItem = Console.ReadLine();
 
             if (int.TryParse(priceOfItem, out int newpriceOfItem))
             {
                 nameOfItems.Add(item);
                 priceOfItems.Add(newpriceOfItem);
-                Console.Clear();
-                for (int i = 0; i < nameOfItems.Count; i++)
-                {   
-                    Console.WriteLine($"{i + 1}. {nameOfItems[i]} - {priceOfItems[i]}kr");
-                }
             }
         }  
     } 
 
-    if(choice == "D")
+    if (choice == "D")
     {
         Console.WriteLine("Type the number in the list that you want to delete.");  
         string input = Console.ReadLine();
-        if(int.TryParse(input, out int itemNumber))
+
+        if (int.TryParse(input, out int itemNumber))
         {
-            if(itemNumber >= 1 && itemNumber <= nameOfItems.Count)
+            if (itemNumber >= 1 && itemNumber <= nameOfItems.Count)
             {
-                int itemPostion = itemNumber -1;
+                int itemPostion = itemNumber - 1;
                 nameOfItems.RemoveAt(itemPostion);
                 priceOfItems.RemoveAt(itemPostion);
-
-            
             }
         }       
     }
-}
 
+    Console.Clear();
+
+    for (int i = 0; i < nameOfItems.Count; i++)
+    {   
+        Console.WriteLine($"{i + 1}. {nameOfItems[i]} - {priceOfItems[i]}kr");
+    }
+}
